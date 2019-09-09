afa8761dbb3c24623e69dd80f6f7690026aa56e1
Jari Silvo <jari.silvo@cs.helsinki.fi>
2019-09-10 02:15:59 +0300

    build pois gitignoresta

---
+++ build/precache-manifest.9891d34565749e996bf3d69c05333566.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cd5c06099835e133afc1a32cd0ff98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73fbec56e5f0407da1f0",
+    "url": "/static/css/main.251313f5.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "73fbec56e5f0407da1f0",
+    "url": "/static/js/main.665b9567.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
